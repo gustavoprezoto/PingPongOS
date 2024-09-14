e4458cdf6b4c50d72b777f564d60d77207eb940c
Gustavo <gustavoprezotoboca@gmail.com>
2024-09-14 16:32:00 -0300

    Implementing scheduler with priority system.

--- src/ppos_core.c
+++ src/ppos_core.c
@@ -16,6 +16,8 @@ int tid;
 
 void dispatcher();
 void print_elem (void *ptr);
+void age_task_dynamic_priority(task_t *task);
+void task_set_base_dynamic_priority(task_t *task);
 
 void ppos_init() {
     // Desabilita o buffer do stdout, usado pelo printf, para previnir race conditions.
@@ -39,7 +41,38 @@ void task_yield() {
 }
 
 task_t* scheduler() {
-    return task_queue;
+    task_t *queue_iterator = task_queue;
+    task_t *next_scheduled_task = task_queue; // iteration strategy purposes
+
+    // Iterate the task queue to find the next scheduled task
+    do {
+        if(task_getprio(queue_iterator) < task_getprio(next_scheduled_task))
+            next_scheduled_task = queue_iterator;
+
+        queue_iterator = queue_iterator->next;
+    } while (queue_iterator != task_queue);
+
+    task_set_base_dynamic_priority(next_scheduled_task);
+
+    // Iterate the task queue again to apply age factor to enqueued tasks
+    queue_iterator = task_queue;
+    do {
+        if(queue_iterator != next_scheduled_task)
+            age_task_dynamic_priority(queue_iterator);
+
+        queue_iterator = queue_iterator->next;
+    } while (queue_iterator != task_queue);
+
+    return next_scheduled_task;
+}
+
+void age_task_dynamic_priority(task_t *task) {
+    if (task->dynamic_priority != PRIORITY_MIN)
+        task->dynamic_priority -= PRIORITY_AGING_FACTOR;
+}
+
+void task_set_base_dynamic_priority(task_t *task) {
+    task->dynamic_priority = DYNAMIC_PRIORITY_BASE_VALUE;
 }
 
 void dispatcher(void *arg) {
@@ -90,6 +123,8 @@ int task_init(task_t *task, void (*start_routine)(void *),  void *arg) {
     tid++;
     task->id = tid;
     task->state = READY;
+    task_setprio(task, 0);
+    task_set_base_dynamic_priority(task);
 
     void* end = (char*)task->context.uc_stack.ss_sp + task->context.uc_stack.ss_size;
     task->vg_id = VALGRIND_STACK_REGISTER (task->context.uc_stack.ss_sp, end);
@@ -121,6 +156,27 @@ int task_switch(task_t *task) {
 
 int task_id() {
     return exec_task->id;
+}
+
+void task_setprio (task_t *task, int prio) {
+    if(task == NULL)
+        task = exec_task;
+
+    if(prio > PRIORITY_MAX)
+        prio = PRIORITY_MAX;
+
+
+    if(prio < PRIORITY_MIN)
+        prio = PRIORITY_MIN;
+
+    task->static_priority = prio;
+}
+
+int task_getprio (task_t *task) {
+    if (task == NULL)
+        task = exec_task;
+
+    return task->static_priority + task->dynamic_priority;
 }
 
 void task_exit(int exit_code) {
--- tests/scheduler_tests/scheduler_test.c
+++ tests/scheduler_tests/scheduler_test.c
@@ -47,6 +47,8 @@ int main (int argc, char *argv[])
     task_init (&Pung, Body, "                    Pung") ;
     task_setprio (&Pung, 8);
 
+    task_yield();
+
     printf ("main: fim\n");
     task_exit (0) ;
 }
